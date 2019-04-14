9fb86859127f989585157d6d414d6bc7700435a0
c10t <c10t@fa.ke>
2019-04-14 23:31:11 +0900

    flake8 fix

--- abs/abc086a.py
+++ abs/abc086a.py
@@ -2,14 +2,17 @@
 
 # ABC086A - Product
 
+
 def main():
     b, c = map(int, input().split())
     print(deer(b, c))
+
 
 def deer(b, c):
     if b * c % 2 == 0:
         return "Even"
     else:
         return "Odd"
+
 
 main()
