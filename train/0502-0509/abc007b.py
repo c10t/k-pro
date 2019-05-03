55bdeaae330b9f9fdfb5f8e0322dca46cd9d924e
c10t <c10t@fa.ke>
2019-05-03 23:54:39 +0900

    2019/05/03

---
+++ train/0502-0509/abc007b.py
@@ -0,0 +1,11 @@
+def main():
+    a = input()
+    if len(a) < 2:
+        if a == 'a':
+            print('-1')
+            return
+        else:
+            print('a')
+            return
+
+    print('a')
