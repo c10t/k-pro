6ef10eca70461bbe8a8df0d14a75c3de68f6445d
c10t <c10t@fa.ke>
2019-08-20 01:54:39 +0900

    0820

--- train/0812-0819/abc137/d.py
+++ train/0812-0819/abc137/d.py
@@ -1,16 +1,27 @@
-import heapq
+from heapq import heappush, heappop
 from operator import itemgetter
 
 
-def main(n, m, jobs):
-    backward = sorted(jobs, key=itemgetter(0), reverse=True)
-    pass
+def main(n, m, works):
+    backward = sorted(works, key=itemgetter(0), reverse=True)
+    q = []
+    score = 0
+    for i in range(1, m + 1):
+        while backward and backward[-1][0] <= i:
+            a, b = backward.pop()
+            heappush(q, -b)
+            # print(q)
+        if q:
+            b = -heappop(q)
+            score += b
+
+    return score
 
 
 n, m = list(map(int, input().split()))
-jobs = []
+works = []
 for _ in range(n):
     a, b = list(map(int, input().split()))
-    jobs.append((a, b))
+    works.append((a, b))
 
-print(main(n, m, jobs))
+print(main(n, m, works))
