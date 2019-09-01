71002d9fa0f43778064dc0cde02ea4f439234f85
c10t <c10t@fa.ke>
2019-09-01 23:57:47 +0900

    0901

---
+++ train/118/b.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+n, m = map(int, input().split())
+
+tastes = np.zeros((n, m), dtype=np.int8)
+for i in range(n):
+    ka = list(map(int, input().split()))
+    for j in range(ka[0]):
+        # print(i, ka[j + 1] - 1)
+        tastes[i][ka[j + 1] - 1] = 1
+
+    # for t in tastes:
+    #     print(t)
+    # print("---")
+
+# tastes = np.array(tastes)
+# print(tastes)
+
+score = np.sum(np.sum(tastes == 1, axis=0) == n)
+print(score)
